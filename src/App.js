bf0e26ed9c3d7911d07ee0ad02d742343a9e5d0e
tsachin34 <jrsachinn99@gmail.com>
2023-03-29 21:52:23 +0545

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
